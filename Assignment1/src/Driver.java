f4c6f0b6b1ed99931493d6e13f4051ce13e1843d
ZeyadMaher <zeyadmaher638@gmail.com>
2021-11-22 23:36:06 +0200

    customer + driver

---
+++ Assignment1/src/Driver.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Driver implements User {
+
+    userState state=userState.PENDING;
+private String license;
+private String national_id;
+private ArrayList<String>fav_area=new ArrayList<String>();
+private ArrayList<Integer>ratings=new ArrayList<Integer>();
+private ArrayList<User>notfication=new ArrayList<User>();
+Offer offer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public User Login() {
+        return null;
+    }
+
+    @Override
+    public void Register() {
+
+    }
+
+}
---
+++ Assignment1/src/User.java
@@ -0,0 +1,5 @@
+public interface User {
+    public User Login();
+    public void Register();
+
+}
